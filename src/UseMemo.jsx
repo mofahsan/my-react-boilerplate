0dbe0fa835380b4917c67305ce02a50134c346a3
Mohd Ahsan <mohdahsan@WILASSETA077.local>
2023-03-07 16:30:07 +0530

    advance usememo example

--- src/UseMemo.jsx
+++ src/UseMemo.jsx
@@ -1,4 +1,4 @@
-import { useState,useMemo } from "react";
+import { useState,useMemo, useEffect } from "react";
 
 export default function UseMemo(){ // The problem is that each time number value changes OR dark theme value changes component rerender as state changes slow function will be triggered again and again causing delay we don't want to recompute dublenumber if we're not changing number value therefore we will cache value of slow function .
        const [number,setNumber] =  useState(0)
@@ -8,10 +8,18 @@ export default function UseMemo(){ // The problem is that each time number value
        },[number])
        
        
-       const themeStyles = {
-        backgroundColor : dark ? 'black' : 'white',
-        color : dark ? 'white' : 'black'
-       }
+       const themeStyles = useMemo(()=>{
+        return {
+            backgroundColor : dark ? 'black' : 'white',
+            color : dark ? 'white' : 'black'
+           }
+       },[dark]) // dark here since we only change themestyle when dark var is changed now useEffect will not work with themeStyles as dependency
+
+       //now if we do useEffect here with themeStyles as dependency
+       useEffect(()=>{
+            console.log("theme styles changed")
+       },[themeStyles])
+       //problem :- this code runs even if we change number value because component rerender and according to js themeStyles and rerendered themeStyles are different therefore we will have to wrap theme styles in usememo as well
 
  return (
     <>
